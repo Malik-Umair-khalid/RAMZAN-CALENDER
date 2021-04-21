6be30f4073dd59638b539083585c7ba7ae50cc9d
Malik-Umair-khalid <81845666+Malik-Umair-khalid@users.noreply.github.com>
2021-04-21 08:02:27 -0700

    cOMPLETED

---
+++ app.js
@@ -0,0 +1,125 @@
+var ramadanDate = +prompt("Which date's sehr and iftar time you want to know");
+
+if (ramadanDate === 1){
+    alert("Sahar    :   4:51 AM \n Iftar      :   6:55 PM")
+   
+}
+else if (ramadanDate === 2){
+    alert("Sahar    :   4:50 AM \n Iftar      :   6:55 PM")
+   
+}
+else if (ramadanDate === 3){
+    alert("Sahar    :   4:49 AM \n Iftar      :   6:56 PM")
+   
+}
+else if (ramadanDate === 4){
+    alert("Sahar    :   4:48 AM \n Iftar      :   6:56 PM")
+   
+}
+else if (ramadanDate === 5){
+    alert("Sahar    :   4:47 AM \n Iftar      :   6:57 PM")
+   
+}
+else if (ramadanDate === 6){
+    alert("Sahar    :   4:46 AM \n Iftar      :   6:57 PM")
+   
+}
+else if (ramadanDate === 7){
+    alert("Sahar    :   4:45 AM \n Iftar      :   6:57 PM")
+   
+}
+else if (ramadanDate === 8){
+    alert("Sahar    :   4:44 AM \n Iftar      :   6:58 PM")
+   
+}
+else if (ramadanDate === 9){
+    alert("Sahar    :   4:43 AM \n Iftar      :   6:58 PM")
+   
+}
+else if (ramadanDate === 10){
+    alert("Sahar    :   4:42 AM \n Iftar      :   6:59 PM")
+   
+}
+else if (ramadanDate === 11){
+    alert("Sahar    :   4:41 AM \n Iftar      :   6:59 PM")
+   
+}
+else if (ramadanDate === 12){
+    alert("Sahar    :   4:40 AM \n Iftar      :   7:00 PM")
+   
+}
+else if (ramadanDate === 13){
+    alert("Sahar    :   4:39 AM \n Iftar      :   7:00 PM")
+   
+}
+else if (ramadanDate === 14){
+    alert("Sahar    :   4:38 AM \n Iftar      :   7:01 PM")
+   
+}
+else if (ramadanDate === 15){
+    alert("Sahar    :   4:37 AM \n Iftar      :   7:01 PM")
+   
+}
+else if (ramadanDate === 16){
+    alert("Sahar    :   4:36 AM \n Iftar      :   7:02 PM")
+   
+}
+else if (ramadanDate === 17){
+    alert("Sahar    :   4:35 AM \n Iftar      :   7:02 PM")
+   
+}
+else if (ramadanDate === 18){
+    alert("Sahar    :   4:34 AM \n Iftar      :   7:03 PM")
+   
+}
+else if (ramadanDate === 19){
+    alert("Sahar    :   4:33 AM \n Iftar      :   7:03 PM")
+   
+}
+else if (ramadanDate === 20){
+    alert("Sahar    :   4:32 AM \n Iftar      :   7:04 PM")
+   
+}
+else if (ramadanDate === 21){
+    alert("Sahar    :   4:31 AM \n Iftar      :   7:04 PM")
+   
+}
+else if (ramadanDate === 22){
+    alert("Sahar    :   4:30 AM \n Iftar      :   7:05 PM")
+   
+}
+else if (ramadanDate === 23){
+    alert("Sahar    :   4:29 AM \n Iftar      :   7:05 PM")
+   
+}
+else if (ramadanDate === 24){
+    alert("Sahar    :   4:28 AM \n Iftar      :   7:06 PM")
+   
+}
+else if (ramadanDate === 25){
+    alert("Sahar    :   4:27 AM \n Iftar      :   7:06 PM")
+   
+}
+else if (ramadanDate === 26){
+    alert("Sahar    :   4:26 AM \n Iftar      :   7:07 PM")
+}
+else if (ramadanDate === 26){
+    alert("Sahar    :   4:25 AM \n Iftar      :   7:07 PM")
+   
+}
+else if (ramadanDate === 27){
+    alert("Sahar    :   4:24 AM \n Iftar      :   7:08 PM")
+   
+}
+else if (ramadanDate === 28){
+    alert("Sahar    :   4:23 AM \n Iftar      :   7:08 PM")
+   
+}
+else if (ramadanDate === 29){
+    alert("Sahar    :   4:22 AM \n Iftar      :   7:09 PM")
+   
+}
+else if (ramadanDate === 30){
+    alert("Sahar    :   4:21 AM \n Iftar      :   7:09 PM")
+   
+}
